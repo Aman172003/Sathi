3bc825ded315ecdd0c0b687806fe40a41981f923
Aman Kumar Rai <tej171995@gmail.com>
2024-03-08 01:24:01 +0530

    latest

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 // jab bhi kuchh load hoga ya phir koi unknown link pe jaega to index file render ho jaega jo react se link hai to koi bhi error nhi aaega
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
 });
 
 const userSocketMap = {};
